79714f2d13b160a38c96d2778c7a74f9ae6e068d
Leexin <leexin29@qq.com>
2022-04-01 14:58:32 +0800

    ll

--- open.py
+++ open.py
@@ -1,1 +1,12 @@
-print ("hello world")
+import cv2 as cv
+
+img = cv.imread('D:\Python\Python project\OpenCV\cat.jpg')
+print(img)
+cv.imshow("cat",img)
+cv.waitKey(0)    
+cv.destroyAllWindows() 
+print(img.shape[0])
+print(img.shape[1])
+print(img.shape[2])
+print(img.shape[:2])
+print(img.shape[:3])
